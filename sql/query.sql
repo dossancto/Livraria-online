091b12f33e7ea8ac14f9b8a3471420fd7a68ce68
lu-css <dossantossilvalucas963@gmail.com>
2022-08-06 16:13:23 -0300

    Aula #10

--- sql/query.sql
+++ sql/query.sql
@@ -1,5 +1,38 @@
+DROP DATABASE db_livraria;
 CREATE DATABASE db_livraria
 DEFAULT CHARACTER SET UTF8
 DEFAULT COLLATE UTF8_GENERAL_CI;
 
 USE db_livraria;
+
+CREATE TABLE tbl_categoria
+(
+	cd_categoria INT PRIMARY KEY AUTO_INCREMENT,
+    ds_categoria VARCHAR(25) NOT NULL
+)
+DEFAULT CHARSET UTF8;
+
+CREATE TABLE tbl_autor
+(
+	cd_autor INT PRIMARY KEY AUTO_INCREMENT,
+    nm_autor VARCHAR(45) NOT NULL
+)
+DEFAULT CHARSET UTF8;
+
+CREATE TABLE tbl_livro
+(
+	cd_livro INT PRIMARY KEY AUTO_INCREMENT,
+    no_isbn VARCHAR(17) NOT NULL,
+    cd_categoria INT NOT NULL,
+    nm_livro VARCHAR(70) NOT NULL,
+    cd_autor INT NOT NULL,
+    no_pag VARCHAR(4) NOT NULL,
+    vl_preco DECIMAL(6,2) NOT NULL,
+    qt_estoque INT NOT NULL,
+    ds_resumo_obra TEXT NOT NULL,
+    ds_capa VARCHAR(255) NOT NULL,
+    sg_lancamento ENUM('S', 'N') NOT NULL,
+    CONSTRAINT fk_cat FOREIGN KEY(cd_categoria) REFERENCES tbl_categoria(cd_categoria),
+    CONSTRAINT fk_autor FOREIGN KEY(cd_autor) REFERENCES tbl_autor(cd_autor)
+)
+DEFAULT CHARSET UTF8;
