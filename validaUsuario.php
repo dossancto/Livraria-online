96843c67065ceb5665c36fd114a687c56837776a
= <=>
2022-08-15 22:55:08 -0300

    Aula #29

--- validaUsuario.php
+++ validaUsuario.php
@@ -2,20 +2,20 @@
 
 include 'sql/conexao.php';
 
+session_start(); // Inicia a sessão
+
+
 $v_usuario = $_POST['txtemail'];
 $v_senha = $_POST['txtsenha'];
-
-// echo $v_usuario . '<br>';
-// echo $v_senha;
 
 $consulta = $connection->query("select cd_usuario, ds_email, ds_senha, ds_status, nm_usuario from tbl_usuario where ds_email = '$v_usuario' and ds_senha = '$v_senha' ");
 
 if($consulta->rowCount() == 1){
-  echo "Cadastrado";
+  $exibeusuario = $consulta->fetch(PDO::FETCH_ASSOC);
+  $_SESSION['ID'] = $exibeusuario['cd_usuario'];
+  header('location:index.php');
 }
 else{
-  echo 'Faça login antes';
+  header('location:erro.php');
+  
 }
-
-
-?>
