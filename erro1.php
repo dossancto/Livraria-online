726e6c89b939172bed1a08abb87fd6d059d7eb52
lu-css <dossantossilvalucas963@gmail.com>
2022-09-04 21:12:41 -0300

    Aula #37

---
+++ erro1.php
@@ -0,0 +1,59 @@
+doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Minha Loja - Logon de usuário</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Jquery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+  <!-- ICONS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+  <style>
+    .navbar {
+      margin-bottom: 0;
+    }
+  </style>
+
+
+</head>
+
+<body>
+
+  <?php
+
+  include 'sql/conexao.php';
+  include 'components/navbar.php';
+
+  ?>
+
+
+  <div class="container-fluid">
+
+    <div class="row">
+
+      <div class="col-sm-4"></div>
+      <div class="col-sm-4 text-center">
+
+        <h2>Email já em uso na Loja!!!</h2>
+
+        <a href="frmusuario.php" class="btn btn-block btn-info" role="button">Tentar Novamente</a>
+
+        <a href="esquecisenha.php" class="btn btn-block btn-primary" role="button">Esqueci a senha</a>
+
+
+      </div>
+    </div>
+  </div>
+
+
+</body>
+
+</html>
--- inserirUsuario.php
+++ inserirUsuario.php
@@ -9,20 +9,20 @@ $endereco = $_POST['txtendereco'];
 $cidade = $_POST['txtcidade'];
 $cep = $_POST['txtcep'];
 
-// /*
+/*
 echo $nome . "<br>";
 echo $email . "<br>";
 echo $senha . "<br>";
 echo $endereco . "<br>";
 echo $cidade . "<br>";
 echo $cep . "<br>";
-// */
+*/
 
 $consulta = $connection->query("select ds_email from tbl_usuario where ds_email = '$email'");
 $existe_usuario = $consulta->rowCount() == 1;
 
 if ($existe_usuario) {
-  // header('location:erro1.php');
+  header('location:erro1.php');
 } else {
   $incluir = $connection->query("
   insert into tbl_usuario
